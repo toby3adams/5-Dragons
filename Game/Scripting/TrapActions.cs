35396323afa9597dc303cb6a2f17c225ea46dc72
Douglas Hill <drhill99@hotmail.com>
2022-07-13 17:15:49 -0600

    Traps go BOOOOOOOOOOM

--- Game/Scripting/TrapActions.cs
+++ Game/Scripting/TrapActions.cs
@@ -9,6 +9,13 @@ namespace Dragons.Game.Scripting
 
     public class TrapActions : Action
     {   
+        private int lava_counter;
+
+        private int pit_fallBottom = -20;
+        private int pit_fallTop = -21;
+
+        private int pit_fallLeft = 0;
+        private int pit_fallRight = 0;
         Random rnd = new Random();
         public TrapActions(){}
 
@@ -33,6 +40,33 @@ namespace Dragons.Game.Scripting
                     }                    
                 }
             }
+             if (this.lava_counter > 65)
+                {
+                    List<Trap> Lava = scene.GetAllActors<Trap>("lava"); 
+
+
+                    foreach (Trap lavaBlock in Lava){
+                        if (player.Overlaps(lavaBlock)){
+                            player.takes_damage(player.damage);
+                        }
+                    }
+                    this.lava_counter=0;
+                }
+                this.lava_counter++;
+
+                //Doesn't need counter
+                    List<Trap> pit_fall = scene.GetAllActors<Trap>("pit");
+
+                    foreach (Trap pit_falls in pit_fall)
+                    {
+                        if(player.FullyOverlaps(pit_falls,pit_fallTop,pit_fallBottom,pit_fallLeft,pit_fallRight))
+                        {
+                           player.takes_damage(player.damage); 
+                        }
+
+
+
+                    }
             
         }
 
